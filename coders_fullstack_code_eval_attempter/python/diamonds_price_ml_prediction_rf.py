cc60b1a0372e8527c54d468f1efb37b8ede0cdea
Alireza <ar.mahdavi21@gmail.com>
2024-12-18 21:16:19 -0500

    Update diamonds_price_ml_prediction_rf.py

--- coders_fullstack_code_eval_attempter/python/diamonds_price_ml_prediction_rf.py
+++ coders_fullstack_code_eval_attempter/python/diamonds_price_ml_prediction_rf.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Dec 18 21:11:48 2024
+Program to perform Random Forest Regressor to predict the price of dimaond over a set of other features
 
 @author: alima
 """
@@ -13,20 +14,20 @@ from sklearn.ensemble import RandomForestRegressor  # For the Random Forest Regr
 from sklearn.metrics import mean_squared_error  # For evaluating the model's performance
 
 # Load the data from a CSV file and drop unnecessary columns
-df = pd.read_csv('Diamonds_Prices.csv').drop(columns={'Unnamed: 0', 'x', 'y', 'z', "cut", "color", "clarity"})
+df = pd.read_csv('Diamonds_Prices.csv').drop(columns = {'Unnamed: 0', 'x', 'y', 'z', 'cut', 'color', 'clarity'})
 
 # Define the feature set (X) by dropping the 'price' column from the dataframe
-X = df.drop(columns=['price'])
+X = df.drop(columns = ['price'])
 
 # Define the target variable (y) which is the 'price' column
 y = df['price']
 
 # Split the data into training and testing sets (80% training, 20% testing)
 # The random_state parameter ensures reproducibility of the split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
 # Initialize the RandomForestRegressor model with 100 trees and a random_state for reproducibility
-model = RandomForestRegressor(n_estimators=100, random_state=42)
+model = RandomForestRegressor(n_estimators = 100, random_state = 42)
 
 # Train the model using the training data
 model.fit(X_train, y_train)
